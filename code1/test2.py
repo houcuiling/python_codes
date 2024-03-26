2f7b14b9890e958800ccb41f569376ad260b1c61
hcl <2090468309@qq.com>
2024-03-26 10:21:43 +0800

    20240326数学运算

---
+++ code1/test2.py
@@ -0,0 +1,48 @@
+# 20240326
+# 数学运算
+
+# 加法例子
+print("加法"，(2+2))
+      
+#减法例子
+print("减法"，(50-30))
+      
+#乘法例子
+print("乘法"，(50-5*6))
+
+ #除法例子
+print("除法"，((50-5*6)/4)
+
+#float 浮点数 （小数）
+xiaoshu=8/5
+print("浮点数"，xiaoshu) 
+      
+#2,4,20都是整数，类型：int
+#5.0，1.6都是浮点数，类型：float
+#除法运算(/)总是返回浮点数。
+
+#floor diversion：向下取整
+print("17/3=",(17/3))
+print("17//3=",(17//3)) #向下取整为5
+
+#取余(%)
+print("17 % 3 =",(17 % 3))
+
+#乘方(**
+print（"5的平方=”，5**2)
+print（"2的7次方=”，2**7)
+
+width = 20 # 宽度
+height = 5 * 9 # 高度
+area = width * height #面积
+width = 3 # 宽度
+height = 4 # 高度
+
+import math
+
+length = math.sqrt(width * width +height * heigth) # 对角线长度
+print(length)
+
+length = (width * width + height * height) ** 0.5
+print(length)
+
